d7290f69a2e68e02714bcf6575fb519ecad86e57
inarus <lionlane@bk.ru>
2016-06-24 19:09:29 +0400

    Создали класс point

--- snake_new/snake_new/Program.cs
+++ snake_new/snake_new/Program.cs
@@ -10,33 +10,59 @@ namespace snake_new
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            point p1 = new point(); // классу point присваим имя p1
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
 
-            Draw(x1, y1, sym1);
+            //убирае так как добавили в класс функцию
+            //Draw (p1.x, p1.y, p1.sym); 
 
-            //Console.SetCursorPosition(x1, y1); удаляем, так как написана нижу функция 'draw'
+            p1.Draw();
+
+            //ВМЕСО ЭТОГО ИСПОЛЬЗУЕМ КЛАСС POINT
+
+            //int x1 = 1; 
+            //int y1 = 3;
+            //char sym1 = '*';
+
+            //Draw(x1, y1, sym1);
+
+            //УБИРАЕМ, ТАК КАК НАПИСАНА ФУНКЦИЯ DRAW
+
+            //Console.SetCursorPosition(x1, y1); 
             //Console.Write(sym1);
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
+            point p2 = new point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            
+            //Draw(p2.x, p2.y, p2.sym);
 
-            Draw(x2, y2, sym2);
+            p2.Draw();
 
-            //Console.SetCursorPosition(x2, y2); удаляем, так как написана нижу функция 'draw'
-            //Console.Write(sym2);
+
+            //int x2 = 4;
+            //int y2 = 5;
+            //char sym2 = '#';
+
+            //Draw(x2, y2, sym2);
+
+            //Console.SetCursorPosition(x2, y2); установка курсора с координатами (4,5)    // удаляем, так как написана нижу функция 'draw' 
+            //Console.Write(sym2); выводим на экран
 
              
-        Console.ReadLine();
+        Console.ReadLine(); //показывать пока не нажмем Enter
         }
 
-        static void Draw(int x, int y, char sym) // объединили две функции в одну
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
 
-        }
+        //УБРАЛИ ТАК СОЗДАЛИ ДАННУЮ ФУНКЦИЮ В КЛАССЕ
+        //static void Draw(int x, int y, char sym) // объединили две функции в одну (отвечает за отображение функции на консоли)
+        //{
+        //    Console.SetCursorPosition(x, y);
+        //    Console.Write(sym);
+
+        //}
     }
 }
---
+++ snake_new/snake_new/point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_new
+{
+    class point // создаем класс point
+    {
+        public int x; // говорим что класс состоит из данных (указываем переменные)
+        public int y;
+        public char sym;
+
+        public void Draw() // говорим что класс состоит из функции
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
+        }
+
+    }
+}

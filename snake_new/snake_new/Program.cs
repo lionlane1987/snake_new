eb5c649863653e724132f189872df40893272a04
inarus <lionlane@bk.ru>
2016-06-24 18:32:15 +0400

    Процедурный подход. Функция

--- snake_new/snake_new/Program.cs
+++ snake_new/snake_new/Program.cs
@@ -10,9 +10,33 @@ namespace snake_new
     {
         static void Main(string[] args)
         {
-        Console.WriteLine("Helo world");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+
+            //Console.SetCursorPosition(x1, y1); удаляем, так как написана нижу функция 'draw'
+            //Console.Write(sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+            //Console.SetCursorPosition(x2, y2); удаляем, так как написана нижу функция 'draw'
+            //Console.Write(sym2);
+
+             
         Console.ReadLine();
         }
 
+        static void Draw(int x, int y, char sym) // объединили две функции в одну
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
+        }
     }
 }
